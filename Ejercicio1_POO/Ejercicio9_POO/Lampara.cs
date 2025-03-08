6159b916edff03a2ba86753bb9eac19cf76b9255
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-07 23:49:07 -0500

    Add Lampara class with methods to control and display its state

---
+++ Ejercicio1_POO/Ejercicio9_POO/Lampara.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Ejercicio9_POO
+{
+    /*Los metodos y los atributos privados
+     *son aquellos que solo pueden ser accedidos
+     *desde la misma clase
+     * que los declara
+     */
+    
+    public class Lampara
+    {
+        private bool encendido;
+        // Metodo Constructor: se llama exactamente igual que la clase es un metodo que se utiliza para inicializar recursos necesarios de tu clase
+        public Lampara()
+        {
+            this.encendido = false;
+        }
+
+        private bool estaEncendido()
+        {
+            return this.encendido;
+        }
+
+        public void encender()
+        {
+            if (!this.encendido)
+            {
+                this.encendido = true;
+                Console.WriteLine("La lampara esta encendida");
+            }
+            else
+            {
+                Console.WriteLine("Lampara ya encendida");
+            }
+        }
+
+        public void apagar()
+        {
+            if (this.encendido) // si esta encendido
+            {
+                this.encendido = false;
+                Console.WriteLine("La lampara se ha apagado");
+            }
+            else
+            {
+                Console.WriteLine("La lampara ya estaba apagada");
+            }
+        }
+        
+        public void mostrarEstado()
+        {
+            if(this.estaEncendido())
+            {
+                Console.WriteLine("La lampara esta encendida");
+            }
+            else
+            {
+                Console.WriteLine("La lampara esta apagada");
+            }
+        }
+    }
+}
