3103bffe5ee13cbd7b946832ea80a98823aedfba
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-07 23:21:51 -0500

    Add Operaciones and TiposDatos classes with basic methods for arithmetic and data types

---
+++ Ejercicio1_POO/Ejercicio6_POO/TiposDatos.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejercicio6_POO
+{
+    public class TiposDatos
+    {
+        public int returnEntero()
+        {
+            return 10;
+        }
+
+        public string returnString()
+        {
+            return "Hola Mundo";
+        }
+
+        public double returnDouble()
+        {
+            return 10.5;
+        }
+
+        public bool returnBoolean()
+        {
+            return true; // false
+        }
+
+        public void returnVoid()
+        {
+            int a = 0; // No retorna nada
+        }
+    }
+}
---
+++ Ejercicio1_POO/Ejercicio7_POO/Operaciones.cs
@@ -0,0 +1,18 @@
+﻿namespace Ejercicio7_POO
+{
+    public class Operaciones
+    {
+        /*
+          Un parametro a una variable
+          que se declara en una funcion o
+          un metodo y el metodo o funcion
+          puede pedir o no el parametro
+        */
+        public double sumar(int valor1 , int valor2, double a)
+        {
+            double resultado = valor1 + valor2 + a;
+            return resultado;
+        }
+
+    }
+}
