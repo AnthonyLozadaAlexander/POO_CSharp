d7e9695a43950d13bbbdd862b65d226c17af4bae
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-08 21:42:25 -0500

    Add Animal and Perro classes to Ejercicio10_POO project

---
+++ Ejercicio1_POO/Ejercicio10_POO/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio10_POO
+{
+    class Animal
+    {     
+        // Atributos
+        protected string _nombre;
+
+        // Metodo constructor
+        public Animal(string nombre) 
+        {
+            _nombre = nombre;
+        }
+
+        public void obtener_nombre()
+        {
+            Console.WriteLine("El nombre del animal es: " + _nombre);
+        }
+        
+        protected void EmitirSonido()
+        {
+            Console.WriteLine("Este animal hace sonido");
+        }
+    }
+    
+}
---
+++ Ejercicio1_POO/Ejercicio10_POO/Perro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio10_POO
+{
+    class Perro : Animal
+    {
+        public Perro(string nombre) : base(nombre)
+        {
+            
+        }
+
+        public void ladrar()
+        {
+            Console.WriteLine(this._nombre + ": Gua Gua");
+        }
+        
+    }
+}
