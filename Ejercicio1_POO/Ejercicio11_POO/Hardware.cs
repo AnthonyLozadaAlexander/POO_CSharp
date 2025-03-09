1314b477cbb3f6ded46dc5967bf26ddc58164495
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-08 22:28:53 -0500

    Add Ejercicio11_POO project with Hardware and Processador classes

---
+++ Ejercicio1_POO/Ejercicio11_POO/Hardware.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio11_POO
+{
+    class Hardware
+    {
+        public string _nombre;
+        public string _fabricante;
+
+        public Hardware(string nombre, string fabricante) // Constructor
+        {
+            _nombre = nombre;
+            _fabricante = fabricante;
+        }
+    }
+
+    class Processador : Hardware
+    {
+        public Processador(string nombre, string fabricante) : base(nombre, fabricante)
+        {
+            
+        }
+        
+    }
+}
